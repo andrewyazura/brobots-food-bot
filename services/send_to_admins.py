60d5aa36e924ecf443ffa8e990ae23a47b0dfde3
andrew4ever <andrewyazura203@gmail.com>
2020-02-24 11:10:02 +0200

    Fix #17 again. Handle errors when sending to admins

--- services/send_to_admins.py
+++ services/send_to_admins.py
@@ -3,7 +3,10 @@ import logging
 
 def send_to_admins(bot, config, message_text, args=(), kwargs={}):
     for admin in config['ADMINS']:
-        bot.send_message(admin, message_text, *args, **kwargs)
+        try:
+            bot.send_message(admin, message_text, *args, **kwargs)
 
-        logging.info('Sent message to admin (%s); message: %s',
-                     admin, message_text)
+            logging.info('Sent message to admin (%s); message: %s',
+                         admin, message_text)
+        except:
+            logging.info('Unable to send message to admin; id: %s', admin)
